aa1cf19d938d58d957bc0eb20da3d6a5a99fc284
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-06-01 22:35:11 +0200

    Sample debug level logging

--- functions/get-index.js
+++ functions/get-index.js
@@ -8,6 +8,8 @@ const http = require('superagent-promise')(require('superagent'), Promise);
 const aws4 = require('../lib/aws4');
 const URL = require('url');
 const log = require('../lib/log');
+const middy = require('middy');
+const sampleLogging = require('../middleware/sample-logging');
 
 const awsRegion = process.env.AWS_REGION;
 const cognitoUserPoolId = process.env.cognito_user_pool_id;
@@ -49,7 +51,7 @@ function* getRestaurants() {
   return (yield httpReq).body;
 }
 
-module.exports.handler = co.wrap(function* (event, context, callback) {
+const handler = co.wrap(function* (event, context, callback) {
   yield aws4.init();
   let template = yield loadHtml();
   log.debug('loaded HTML template');
@@ -80,3 +82,6 @@ module.exports.handler = co.wrap(function* (event, context, callback) {
 
   callback(null, response);
 });
+
+module.exports.handler = middy(handler)
+  .use(sampleLogging({ sampleRate: 0.01 }));
