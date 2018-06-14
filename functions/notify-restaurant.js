aea0d94b376262cdb0b4de456e758164e0abec0b
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-06-14 21:56:52 +0200

    Record custom metrics synchronously

--- functions/notify-restaurant.js
+++ functions/notify-restaurant.js
@@ -4,6 +4,9 @@ const co = require('co');
 const getRecords = require('../lib/kinesis').getRecords;
 const notify = require('../lib/notify');
 const retry = require('../lib/retry');
+const middy = require('middy');
+const sampleLogging = require('../middleware/sample-logging');
+const flushMetrics = require('../middleware/flush-metrics');
 
 module.exports.handler = co.wrap(function* (event, context, cb) {
   let records = getRecords(event);
@@ -19,3 +22,7 @@ module.exports.handler = co.wrap(function* (event, context, cb) {
   
   cb(null, "all done");
 });
+
+module.exports.handler = middy(handler)
+  .use(sampleLogging({ sampleRate: 0.01 }))
+  .use(flushMetrics);
